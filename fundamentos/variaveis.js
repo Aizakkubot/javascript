5d27b2f08984d882647d0e73da5c91b9747f5b3a
isaac <lofikaneko@gmail.com>
2026-04-08 22:23:00 -0300

    variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,47 @@
+/**
+ * Estudo das Variaveis
+ * @autor isaac
+ */
+
+console.clear()
+console.log("estudo das variaveis")
+console.log("")
+console.log("string")
+// a linha abaixo cria variavel
+let nome = "isaac"
+console.log(nome)
+// a linha abaixo mostra o tipo de variavel
+console.log(typeof(nome))
+console.log("")
+console.log("variavel do tipo number")
+let idade = 57
+console.log(idade)
+console.log(typeof(idade))
+let peso = 70
+console.log("")
+
+console.log(peso)
+console.log(typeof(peso))
+console.log("")
+
+let altura = 1.80
+console.log(altura)
+console.log(typeof(altura))
+console.log("")
+
+console.log("variavel do tipo bolean")
+let vip = true
+console.log(vip)
+console.log(typeof(vip))
+
+console.log("")
+console.log("--------------------------")
+console.log("     ficha do aluno"       )
+console.log("--------------------------")
+// a linha abaixo concatena (uniao) um texto com um contexto da variael
+console.log(`nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(`vip: ${vip}`)
+console.log("--------------------------")
